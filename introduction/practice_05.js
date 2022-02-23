9f45cb0249ac2373ae6ffd48dff854bb1b7d890a
hanstar07 <hanstar07@gmail.com>
2022-02-23 23:06:03 +0900

    first commit

---
+++ introduction/practice_05.js
@@ -0,0 +1,7 @@
+console.log('name:%s', 'kyeongrok');
+console.log('age:%d', 31);
+console.log('pi:%d', 3.14);
+console.log('math:%d sscience:%d', 92, 84);
+console.log('name:%s age:%d', 'kyeongrok', 31);
+console.log('name:%s math:%d sscience:%d', 'kyeongrok', 92, 84);
+console.log('name:%s math:%s sscience:%s', 'kyeongrok', 92, 84);
